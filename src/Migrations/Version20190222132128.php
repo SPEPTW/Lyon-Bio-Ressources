158f203aaea1ea23c2acf7dd1f048fc4d22956e3
SPEPTW <sebastien@xboxlive.fr>
2019-02-22 14:28:03 +0100

    Fix Migration

--- src/Migrations/Version20190222132128.php
+++ src/Migrations/Version20190222132128.php
@@ -10,7 +10,7 @@ use Doctrine\Migrations\AbstractMigration;
 /**
  * Auto-generated Migration: Please modify to your needs!
  */
-final class Version20190222125900 extends AbstractMigration
+final class Version20190222132128 extends AbstractMigration
 {
     public function getDescription() : string
     {
