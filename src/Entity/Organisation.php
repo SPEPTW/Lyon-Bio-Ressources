0879674e9526ee7d351e91127639f506cb5264f5
SPEPTW <sebastien@xboxlive.fr>
2019-02-22 15:02:52 +0100

    add entity Organisation

---
+++ src/Entity/Organisation.php
@@ -0,0 +1,74 @@
+<?php
+
+namespace App\Entity;
+
+use Doctrine\ORM\Mapping as ORM;
+
+/**
+ * @ORM\Entity(repositoryClass="App\Repository\OrganisationRepository")
+ */
+class Organisation
+{
+    /**
+     * @ORM\Id()
+     * @ORM\GeneratedValue()
+     * @ORM\Column(type="integer")
+     */
+    private $id;
+
+    /**
+     * @ORM\Column(type="string", length=255)
+     */
+    private $nom;
+
+    /**
+     * @ORM\Column(type="text", nullable=true)
+     */
+    private $Description;
+
+    /**
+     * @ORM\Column(type="integer", nullable=true)
+     */
+    private $Type;
+
+    public function getId(): ?int
+    {
+        return $this->id;
+    }
+
+    public function getNom(): ?string
+    {
+        return $this->nom;
+    }
+
+    public function setNom(string $nom): self
+    {
+        $this->nom = $nom;
+
+        return $this;
+    }
+
+    public function getDescription(): ?string
+    {
+        return $this->Description;
+    }
+
+    public function setDescription(?string $Description): self
+    {
+        $this->Description = $Description;
+
+        return $this;
+    }
+
+    public function getType(): ?int
+    {
+        return $this->Type;
+    }
+
+    public function setType(?int $Type): self
+    {
+        $this->Type = $Type;
+
+        return $this;
+    }
+}
