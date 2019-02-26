5d9437aa2a82e68bd2266068f10f48e8698763f8
Hugo <hros38@gmail.com>
2019-02-26 10:50:37 +0100

    Adding sidebar links

---
+++ templates/_partials/_sidebar.php
@@ -0,0 +1,23 @@
+<section id="sidebar">
+
+    <h4><a href="{{ path('lbr_contact_index') }}">Contacts</a></h4>
+    <ul class="list-group">
+        <li class="list-group-item active"><a href="{{ path('lbr_contact_new') }}">Nouveau contact</a></li>
+    </ul>
+
+    <h4><a href="{{ path('lbr_organisation_index') }}">Organisation</a></h4>
+    <ul class="list-group">
+        <li class="list-group-item active"><a href="{{ path('lbr_organisation_new') }}">Nouvelle organisation</a></li>
+    </ul>
+
+    <h4><a href="{{ path('lbr_evenement_index') }}">Evénements</a></h4>
+    <ul class="list-group">
+        <li class="list-group-item active"><a href="{{ path('lbr_evenement_new') }}">Nouvel Evénement</a></li>
+    </ul>
+
+    <h4><a href="{{ path('lbr_categorie_index') }}">Catégories</a></h4>
+    <ul class="list-group">
+        <li class="list-group-item active"><a href="{{ path('lbr_categorie_new') }}">Nouvelle catégorie</a></li>
+    </ul>
+
+</section> 
