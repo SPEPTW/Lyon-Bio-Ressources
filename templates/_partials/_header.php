a4d6c2b0d9e3e6b00c032a5d991301adfdd4ccad
Hugo <hros38@gmail.com>
2019-02-27 15:03:14 +0100

    Mooving assets files in public

--- templates/_partials/_header.php
+++ templates/_partials/_header.php
@@ -1,18 +1,4 @@
-<!DOCTYPE html>
-<html>
 
-<head>
-    <meta charset="UTF-8">
-    <title>{% block title %}Lyon Bio Ressource{% endblock %}</title>
-    {% block stylesheets %}
-    <!--     Bootstrap css -->
-    <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.3.1/css/bootstrap.min.css" integrity="sha384-ggOyR0iXCbMQv3Xipma34MD+dH/1fQ784/j6cY/iJTQUOhcWr7x9JvoRxT2MZw1T" crossorigin="anonymous">
-    <!-- CSS -->
-    <link href="css/main.css" type="text/css" rel="stylesheet">
-    {% endblock %}
-</head>
-
-<body>
     <nav class="navbar navbar-dark fixed-top bg-dark flex-md-nowrap p-0 shadow">
         <img class="navbar-brand col-sm-3 col-md-2 mr-0" src="Lyon-Bio-Ressources\images\logo-LyonBioRessources-1.jpg">
 
