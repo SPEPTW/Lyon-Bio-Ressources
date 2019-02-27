ad03d16e1407e1b208b90277f3729377adf8b55c
Hugo <hros38@gmail.com>
2019-02-27 17:15:50 +0100

    Completing CSS

--- templates/_partials/_header.php
+++ templates/_partials/_header.php
@@ -1,9 +1,11 @@
-<nav class="navbar navbar-dark fixed-top bg-dark flex-md-nowrap p-0 shadow">
-    <img class="navbar-brand col-sm-3 col-md-2 mr-0" src="{{ asset('dist/img/logo.jpg') }}">
+<header>
+        <nav class="navbar navbar-dark fixed-top bg-dark flex-md-nowrap p-0 shadow">
+            <img class="navbar-brand col-sm-3 col-md-2 mr-0" src="{{ asset('dist/img/logo.jpg') }}">
 
-    <ul class=" navbar-nav px-3">
-    <li class="nav-item text-nowrap">
-        <a class="nav-link text-muted" href="{{ path('logout')}}">Se déconnecter</a>
-    </li>
-    </ul>
-</nav> 
+            <ul class=" navbar-nav px-3">
+                <li class="nav-item text-nowrap">
+                    <a class="nav-link text-muted" href="{{ path('logout')}}">Se déconnecter</a>
+                </li>
+            </ul>
+        </nav>
+</header> 
