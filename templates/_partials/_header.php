b4d74d1c8fbbb3809d7b641bece4773f4fd56169
safik <safik@hotmail.fr>
2019-02-27 16:20:10 +0100

    crudresponsive

--- templates/_partials/_header.php
+++ templates/_partials/_header.php
@@ -13,8 +13,8 @@
 </head>
 
 <body>
-    <nav class="navbar navbar-dark fixed-top bg-dark flex-md-nowrap p-0 shadow">
-        <img class="navbar-brand col-sm-3 col-md-2 mr-0" src="Lyon-Bio-Ressources\images\logo-LyonBioRessources-1.jpg">
+    <nav class="navbar navbar-dark fixed-top bg-dark flex-md-nowrap p-0 shadow" img src="{{asset('dist/img/logo-LyonBioRessources-1.jpg')}}">
+        <img class="navbar-brand col-sm-3 col-md-2 mr-0">
 
         <ul class="navbar-nav px-3">
             <li class="nav-item text-nowrap">
