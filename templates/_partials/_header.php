ec2d39d5e08ba6f6229211e22354c1f2e922b01f
Hugo <hros38@gmail.com>
2019-02-28 17:55:02 +0100

    Adjusting CSS

--- templates/_partials/_header.php
+++ templates/_partials/_header.php
@@ -1,7 +1,6 @@
 <header>
     <nav class=" navbar navbar-dark fixed-top bg-dark flex-md-nowrap p-0 shadow">
         <img class="navbar-brand col-sm-3 col-md-2 mr-0" src="{{ asset('dist/img/logo.jpg') }}">
-
         <ul class=" navbar-nav px-3">
             <li class="nav-item text-nowrap">
                 <a class="nav-link text-muted" href="{{ path('logout')}}">Se déconnecter</a>
