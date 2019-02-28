93be9e4923c48be3329d7d15fa528fa76967350c
cedricbd <cebuisso@gmail.com>
2019-02-28 15:08:31 +0100

    fix image

--- templates/_partials/_header.php
+++ templates/_partials/_header.php
@@ -2,7 +2,7 @@
     <nav class=" navbar navbar-dark fixed-top bg-dark flex-md-nowrap p-0 shadow">
         <img class="navbar-brand col-sm-3 col-md-2 mr-0" src="{{ asset('dist/img/logo.jpg') }}">
 
-        <ul class=" navbar-nav px-3">
+        <ul class=" navbar-nav px-3 mx-5">
             <li class="nav-item text-nowrap">
                 <a class="nav-link text-muted" href="{{ path('logout')}}">Se déconnecter</a>
             </li>
