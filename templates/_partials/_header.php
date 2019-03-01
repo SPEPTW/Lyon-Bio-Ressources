5889469423e2f7b72a75b557bea79b16858f9307
Hugo <hros38@gmail.com>
2019-03-01 13:59:51 +0100

    Closing div

--- templates/_partials/_header.php
+++ templates/_partials/_header.php
@@ -1,17 +1,18 @@
 <header>
     <nav class=" navbar navbar-dark fixed-top bg-dark flex-md-nowrap p-0 shadow">
 
-        <img class="navbar-brand col-sm-3 col-md-2 mr-0" src="{{ asset('dist/img/logo.jpg') }}">
+        <img alt="logo" class="navbar-brand col-sm-3 col-md-2 mr-0" src="{{ asset('dist/img/logo.jpg') }}" />
 
         <div>
             <ul class=" navbar-nav px-3">
                 <li id="menu" class="position-fixed d-md-none">
-                    <img src="{{ asset('dist/img/menu.png') }}" alt="Menu">
+                    <img src="{{ asset('dist/img/menu.png') }}" alt="Menu" />
                 </li>
                 <li class="nav-item text-nowrap ">
                     <a class="nav-link text-muted" href="{{ path('logout')}}">Se déconnecter</a>
                 </li>
 
             </ul>
+        </div>
     </nav>
 </header> 
