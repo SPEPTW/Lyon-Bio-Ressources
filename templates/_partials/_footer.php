94560c80960dbebbd991e127dc702a4906681328
Hugo <hros38@gmail.com>
2019-03-01 13:35:04 +0100

    Change contact button

--- templates/_partials/_footer.php
+++ templates/_partials/_footer.php
@@ -1,10 +1,3 @@
-<!-- <footer class="blog-footer mt-auto py-3">
-    <div class="container">
-        <a href="https://www.linkedin.com/company/lyon-bio-ressources/?originalSubdomain=fr" target="_blank"><img src="{{ asset('dist/img/linkedin.png') }}" alt="linkedin" height="42" width="42"></a>
-        <a href="https://www.facebook.com/lyonbioressources/" target="_blank"><img src="{{ asset('dist/img/facebook.png') }}" alt="facebook" height="42" width="42"></a>
-    </div>
-</footer> -->
-
 
 <script src="https://cdnjs.cloudflare.com/ajax/libs/jquery/3.3.1/jquery.min.js" integrity="sha256-FgpCb/KJQlLNfOu91ta32o/NMZxltwRo8QtmkMRdAu8=" crossorigin="anonymous"></script>
 <script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.14.6/umd/popper.min.js" integrity="sha384-wHAiFfRlMFy6i5SRaxvfOCifBUQy1xHdJ/yoi7FRNXMRBu5WHdZYu1hA6ZOblgut" crossorigin="anonymous"></script>
