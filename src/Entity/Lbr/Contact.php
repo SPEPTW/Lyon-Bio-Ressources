be66d334b56f689add8d232ed34a225e325e92fa
cedricbd <cebuisso@gmail.com>
2019-03-04 12:12:04 +0100

    modification bdd code postal and tel in string

--- src/Entity/Lbr/Contact.php
+++ src/Entity/Lbr/Contact.php
@@ -5,6 +5,7 @@ namespace App\Entity\Lbr;
 use Doctrine\Common\Collections\ArrayCollection;
 use Doctrine\Common\Collections\Collection;
 use Doctrine\ORM\Mapping as ORM;
+use Symfony\Component\Validator\Constraints as Assert;
 
 /**
  * @ORM\Entity(repositoryClass="App\Repository\Lbr\ContactRepository")
@@ -20,31 +21,40 @@ class Contact
 
     /**
      * @ORM\Column(type="string", length=50)
+     * @Assert\Length(min=2, max=50)
      */
     private $nom;
 
     /**
      * @ORM\Column(type="string", length=50, nullable=true)
+     * @Assert\Length(max=50)
      */
     private $prenom;
 
     /**
      * @ORM\Column(type="string", length=150, nullable=true)
+     * @Assert\Email(
+     * message = "L'Email'{{ value }}' n'ai pas une adresse mail valide.")
+     * 
      */
     private $email;
 
     /**
      * @ORM\Column(type="string", length=255, nullable=true)
+     * @Assert\Length(max=255)
+     * 
      */
     private $rue;
 
     /**
      * @ORM\Column(type="string", length=255, nullable=true)
+     * @Assert\Length(max=255)
      */
     private $ville;
 
     /**
-     * @ORM\Column(type="integer", nullable=true)
+     * @Assert\Regex("/^[0-9]{5}$/")
+     * @ORM\Column(type="string", nullable=true)
      */
     private $code_postale;
 
@@ -69,17 +79,20 @@ class Contact
     private $evenement;
 
     /**
-     * @ORM\Column(type="integer", nullable=true)
+     * @ORM\Column(type="string", nullable=true)
+     * @Assert\Regex("/^[0-9]{10}$/")
      */
     private $tel_1;
 
     /**
-     * @ORM\Column(type="integer", nullable=true)
+     * @ORM\Column(type="string", nullable=true)
+     * @Assert\Regex("/^[0-9]{10}$/")
      */
     private $tel_2;
 
     /**
-     * @ORM\Column(type="integer", nullable=true)
+     * @ORM\Column(type="string", nullable=true)
+     * @Assert\Regex("/^[0-9]{10}$/")
      */
     private $tel_3;
 
@@ -153,12 +166,12 @@ class Contact
         return $this;
     }
 
-    public function getCodePostale(): ?int
+    public function getCodePostale(): ?string
     {
         return $this->code_postale;
     }
 
-    public function setCodePostale(?int $code_postale): self
+    public function setCodePostale(?string $code_postale): self
     {
         $this->code_postale = $code_postale;
 
@@ -227,36 +240,36 @@ class Contact
         return $this;
     }
 
-    public function getTel1(): ?int
+    public function getTel1(): ?string
     {
         return $this->tel_1;
     }
 
-    public function setTel1(?int $tel_1): self
+    public function setTel1(?string $tel_1): self
     {
         $this->tel_1 = $tel_1;
 
         return $this;
     }
 
-    public function getTel2(): ?int
+    public function getTel2(): ?string
     {
         return $this->tel_2;
     }
 
-    public function setTel2(?int $tel_2): self
+    public function setTel2(?string $tel_2): self
     {
         $this->tel_2 = $tel_2;
 
         return $this;
     }
 
-    public function getTel3(): ?int
+    public function getTel3(): ?string
     {
         return $this->tel_3;
     }
 
-    public function setTel3(?int $tel_3): self
+    public function setTel3(?string $tel_3): self
     {
         $this->tel_3 = $tel_3;
 
