2143573a2645a127184e7171a4686fc3612b950b
cedricbd <cebuisso@gmail.com>
2019-03-05 11:54:35 +0100

    repeatedType Password

--- src/Entity/Lbr/User.php
+++ src/Entity/Lbr/User.php
@@ -6,9 +6,10 @@ use Doctrine\ORM\Mapping as ORM;
 use Symfony\Bridge\Doctrine\Validator\Constraints\UniqueEntity;
 use Symfony\Component\Security\Core\User\UserInterface;
 
+
 /**
  * @ORM\Entity(repositoryClass="App\Repository\Lbr\UserRepository")
- * @UniqueEntity(fields={"email"}, message="There is already an account with this email")
+ * @UniqueEntity(fields={"email"}, message="Un utilisateur existe déjà avec cet email")
  */
 class User implements UserInterface
 {
@@ -21,6 +22,7 @@ class User implements UserInterface
 
     /**
      * @ORM\Column(type="string", length=180, unique=true)
+     * 
      */
     private $email;
 
