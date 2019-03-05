e7b208a27d707987f51967146e8f96737e85424e
cedricbd <cebuisso@gmail.com>
2019-03-05 13:15:20 +0100

    modification user

--- src/Entity/Lbr/User.php
+++ src/Entity/Lbr/User.php
@@ -24,7 +24,7 @@ class User implements UserInterface
     /**
      * @ORM\Column(type="string", length=180, unique=true)
      * @Assert\Email(
-     * message = "L'adresse mail '{{ value }}' n'est pas valide."
+     * message = "L'adresse mail '{{ value }}' n'est pas valide.")
      * 
      */
     private $email;
